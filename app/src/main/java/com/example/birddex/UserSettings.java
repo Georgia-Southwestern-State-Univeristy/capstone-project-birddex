f875704e9c558cb8a04654096b78ec34dfc14b74
zpayne254 <zpayne@radar.gsw.edu>
2026-02-10 17:08:27 -0500

    fixed dark mode issue and added logout

---
+++ app/src/main/java/com/example/birddex/UserSettings.java
@@ -0,0 +1,11 @@
+package com.example.birddex;
+
+public class UserSettings {
+    public boolean notificationsEnabled = false;
+
+    public UserSettings() { }
+
+    public UserSettings(boolean notificationsEnabled) {
+        this.notificationsEnabled = notificationsEnabled;
+    }
+}
